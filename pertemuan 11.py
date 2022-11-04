a99faa3d15415e851f3417bd1b8f89bfbb412c93
Wiliams7 <117442712+Wiliams7@users.noreply.github.com>
2022-11-04 17:03:02 +0700

    Add files via upload

---
+++ pertemuan 11.py
@@ -0,0 +1,67 @@
+# def pilihmenu():
+#     print('silahkan pilih menu')
+#     print('1. tarik tunai')
+#     print('2. setor')
+#     print('3. cek saldo')
+#     print('4. keluar')
+#     pilihan = input('tekan nomor pilihan ')
+#     return (pilihan)
+# print('xxx')
+
+# pilihan = pilihmenu()
+# print(pilihan)
+
+# if int(pilihan) == 1:
+#     print ('tarik tunai')
+# elif int(pilihan) == 2 :
+#     print ('setor')
+# elif int(pilihan) == 3 :
+#     print ('cek saldo')
+# elif int(pilihan) == 4 :
+#     print ('keluar')
+# else:
+#     print('coba lagi')
+# #clearxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# def print_nama (nama):
+#     for i in nama:
+#         print ("nama :"+i)
+# mahasiswa = ['will', 'valen','pota']
+# print_nama(mahasiswa)
+#clearxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# def print_nama(nama):
+#     for i in nama:
+#         salam(i)
+# def salam(nama):
+#     print('hai' + nama)
+    
+# mahasiswa = ['will','valen','pota']
+# print_nama(mahasiswa)
+# #clearxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# def bandingkan (bill1,bill2):
+#     if(bill1 > bill2)
+#     elif(bill)
+#gk clearxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# def vol_balok(p,l,t):
+#     return p*l*t
+
+# panjang = 10
+# lebar = 20
+# tinggi = 30
+# volumebalok = vol_balok(panjang, lebar, tinggi)
+# print(volumebalok)
+# #clearxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# def recursive_number (angka):
+#     print(angka)
+#     if(angka > 0):
+#         recursive_number(angka - 1)
+# recursive_number(5)
+# #clearxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# Tugas:
+#     1. palindrom (kasur rusak) dibolak balik tetep sama
+#     2.anagram (gula & lagu) hurutnya sama tapi urutannya berbeda
